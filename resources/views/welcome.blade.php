f2286e34f7f049b209ee9190ab981e423f09f11e
vimukthi <vimukthi@ceylonit.com>
2018-05-11 14:10:55 +0530

    ffffffff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    welcome master Laravel
+                    Welcome Laravel
 
 
                 <div class="links">
