db633cd580d56fecfe985376d2a0622fb7911faf
vimukthi <vimukthi@ceylonit.com>
2018-05-11 12:28:16 +0530

    twek in welcome master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    welcome Laravel
+                    welcome master Laravel
                 </div>
 
                 <div class="links">
